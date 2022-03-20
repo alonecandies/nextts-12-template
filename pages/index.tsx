24f13cb59877fa83125a422f378978da89ab176c
alonecandies <dragonfrezze@gmail.com>
2022-03-21 02:40:01 +0700

    add page for entry /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import Meta from 'layouts/Meta';
 import type { NextPage } from 'next';
+import DefaultTemplate from 'templates/DefaultTemplate';
 const Home: NextPage = () => {
-	return <></>;
+	return (
+		<DefaultTemplate meta={<Meta title="Home Page" description="This is description" />}>
+			<h1>Home Page</h1>
+		</DefaultTemplate>
+	);
 };
 
 export default Home;
