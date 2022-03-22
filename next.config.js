55cd43070ebd7433f3a5541ceea65962bc1979a7
alonecandies <dragonfrezze@gmail.com>
2022-03-22 07:12:47 +0700

    add package, config env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: ".next",
+  images: {
+    domains: ["cdn.example.com"],
+  }
 }
 
 module.exports = nextConfig
