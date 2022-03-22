e10334dfd8200f1cbc14c192a56d4b934641fffe
alonecandies <dragonfrezze@gmail.com>
2022-03-22 07:17:11 +0700

    update default template

--- templates/DefaultTemplate/index.tsx
+++ templates/DefaultTemplate/index.tsx
@@ -1,21 +1,31 @@
 import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import ScrollToTop from 'components/ScrollToTop';
+import Loading from 'components/Loading';
 
 type IDefaultProps = {
 	meta: ReactNode;
 	children: ReactNode;
 };
 
-const DefaultTemplate = ({ meta, children }: IDefaultProps) => (
-	<>
-		{meta}
-		<Header />
-		{children}
-		<ScrollToTop />
-		<Footer />
-	</>
-);
+const DefaultTemplate = ({ meta, children }: IDefaultProps) => {
+	const router = useRouter();
+
+	if (router.isFallback) {
+		return <Loading />;
+	}
+
+	return (
+		<>
+			{meta}
+			<Header />
+			{children}
+			<ScrollToTop />
+			<Footer />
+		</>
+	);
+};
 
 export default DefaultTemplate;
