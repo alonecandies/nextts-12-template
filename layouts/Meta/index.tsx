a70ec71f951b101a33a87199db566770b70e19cd
alonecandies <dragonfrezze@gmail.com>
2022-03-22 07:20:12 +0700

    update Meta layout

--- layouts/Meta/index.tsx
+++ layouts/Meta/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { NextSeo } from 'next-seo';
-import { metaConfig } from '../../configs/metaConfig';
+import { useRouter } from 'next/router';
+import { metaConfig } from 'configs';
 
 type IMetaProps = {
 	title: string;
@@ -8,26 +9,60 @@ type IMetaProps = {
 	canonical?: string;
 };
 
-const Meta = (props: IMetaProps) => (
-	<>
-		<Head>
-			<meta charSet="UTF-8" key="charset" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
-			<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-			<meta httpEquiv="Cache-Control" content="max-age=31536000, must-revalidate" />
-		</Head>
-		<NextSeo
-			{...metaConfig}
-			title={props.title}
-			description={props.description}
-			canonical={props.canonical}
-			openGraph={{
-				url: props.canonical,
-				title: props.title,
-				description: props.description,
-			}}
-		/>
-	</>
-);
+const Meta = (props: IMetaProps) => {
+	const router = useRouter();
+	return (
+		<>
+			<Head>
+				<meta charSet="UTF-8" key="charset" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
+				<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+				<meta httpEquiv="Cache-Control" content="max-age=31536000, must-revalidate" />
+				<link rel="manifest" href={`${router.basePath}/site.webmanifest`} />
+				<link rel="icon" href={`${router.basePath}/favicon.ico`} key="favicon" />
+				<link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="192x192"
+					href={`${router.basePath}/android-chrome-192x192.png`}
+					key="android"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="512x512"
+					href={`${router.basePath}/android-chrome-512x512.png`}
+					key="android-512"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href={`${router.basePath}/favicon-32x32.png`}
+					key="favicon32"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href={`${router.basePath}/favicon-16x16.png`}
+					key="favicon16"
+				/>
+			</Head>
+			<NextSeo
+				{...metaConfig}
+				title={props.title}
+				description={props.description}
+				canonical={props.canonical}
+				openGraph={{
+					url: props.canonical,
+					title: props.title,
+					description: props.description,
+				}}
+			/>
+		</>
+	);
+};
 
 export default Meta;
