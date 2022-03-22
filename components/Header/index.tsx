870d0f769aeb38a569ca3c8808a0053eeeecaf5a
alonecandies <dragonfrezze@gmail.com>
2022-03-22 07:19:08 +0700

    update header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,34 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import Link from 'next/link';
+import Image from 'next/image';
+import { ROUTES } from 'configs/constants';
+import logo from 'public/assets/images/logo-placeholder.png';
 // import styles from "./styles.module.scss";
 
 const Header = () => (
-   <header>
-      <Navbar bg="dark" variant="dark">
-         <Navbar.Brand href="/">
-            Next App
-         </Navbar.Brand>
-         <Nav.Link>Posts</Nav.Link>
-         <Nav.Link>About</Nav.Link>
-      </Navbar>
-   </header>
-)
+	<header className="bg-dark">
+		<Container>
+			<Navbar variant="dark">
+				<Link href={ROUTES.home} passHref>
+					<Navbar.Brand>
+						<Image src={logo} height={30} width={100} alt="next-logo" />
+					</Navbar.Brand>
+				</Link>
+				<Link href={ROUTES.home} passHref>
+					<Nav.Link>Home</Nav.Link>
+				</Link>
+				<Link href={ROUTES.about} passHref>
+					<Nav.Link>About</Nav.Link>
+				</Link>
+				<Link href={ROUTES.posts} passHref>
+					<Nav.Link>Post</Nav.Link>
+				</Link>
+				<Link href={ROUTES.randomJoke} passHref>
+					<Nav.Link>Jokes</Nav.Link>
+				</Link>
+			</Navbar>
+		</Container>
+	</header>
+);
 
 export default Header;
