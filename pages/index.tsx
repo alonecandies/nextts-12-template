9031c329fbd8e48388034dcb1d7c0c4ea270eb4a
alonecandies <dragonfrezze@gmail.com>
2022-03-22 07:23:58 +0700

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import Meta from 'layouts/Meta';
 import type { NextPage } from 'next';
+import Meta from 'layouts/Meta';
 import DefaultTemplate from 'templates/DefaultTemplate';
+import { Button, Container } from 'react-bootstrap';
+import Link from 'next/link';
+import { ROUTES } from 'configs/constants';
+
 const Home: NextPage = () => {
 	return (
-		<DefaultTemplate meta={<Meta title="Home Page" description="This is description" />}>
-			<h1>Home Page</h1>
+		<DefaultTemplate meta={<Meta title="Home Page" description="This is home description" />}>
+			<Container>
+				<h1 className="text-center">Home Page</h1>
+				<Link href={ROUTES.posts} passHref>
+					<Button variant="primary">Posts</Button>
+				</Link>
+			</Container>
 		</DefaultTemplate>
 	);
 };
